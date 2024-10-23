85b4eded49b91128bf0c859bceb7c355d710ae2a
MrEcstsy <57694923+MrEcstsy@users.noreply.github.com>
2024-10-23 00:50:50 -0700

    New methods
    * Added new helper methods
    * Fixed createLootboxItem method having an improperly made namespace nbt

--- src/ecstsy/LoveLootbox/utils/Utils.php
+++ src/ecstsy/LoveLootbox/utils/Utils.php
@@ -128,4 +128,152 @@ class Utils {
         return $material;
     }
     
+    public static function setupRewards(array $rewardData, ?Player $player = null): array
+    {
+        $rewards = [];
+        $stringToItemParser = StringToItemParser::getInstance();
+        
+        foreach ($rewardData as $data) {
+            if (!isset($data["item"])) {
+                continue; 
+            }
+
+            $display = $data["display"] ?? false;
+            if ($display === true && isset($data["command"]) && $player !== null) {
+                $commandString = str_replace("{player}", $player->getName(), $data["command"]);
+                Server::getInstance()->dispatchCommand(
+                    new ConsoleCommandSender(Server::getInstance(), Server::getInstance()->getLanguage()),
+                    $commandString
+                );
+                continue;
+            }
+
+            $itemString = $data["item"];
+            $item = $stringToItemParser->parse($itemString);
+            if ($item === null) {
+                continue;
+            }
+
+            $amount = $data["amount"] ?? 1;
+            $item->setCount($amount);
+
+            $name = $data["name"] ?? null;
+            if ($name !== null) {
+                $item->setCustomName(C::colorize($name));
+            }
+
+            $lore = $data["lore"] ?? null;
+            if ($lore !== null) {
+                $coloredLore = array_map(function ($line) {
+                    return C::colorize($line);
+                }, $lore);
+                $item->setLore($coloredLore);
+            }
+
+            $enchantments = $data["enchantments"] ?? [];
+            foreach ($enchantments as $enchantmentData) {
+                $enchantmentString = $enchantmentData["enchant"] ?? null;
+                $level = $enchantmentData["level"] ?? 1;
+
+                if ($enchantmentString !== null) {
+                    $enchantment = StringToEnchantmentParser::getInstance()->parse($enchantmentString);
+                    if ($enchantment !== null) {
+                        $item->addEnchantment(new EnchantmentInstance($enchantment, $level));
+                    }
+                }
+            }
+
+            $nbtData = $data["nbt"] ?? [];
+            foreach ($nbtData as $tag => $value) {
+                if (is_int($value)) {
+                    $item->getNamedTag()->setInt($tag, $value);
+                } else {
+                    $item->getNamedTag()->setString($tag, $value);
+                }
+            }
+
+            $rewards[] = $item;
+        }
+
+        return $rewards;
+    }
+
+    public static function startLootboxAnimation(Player $player, InvMenu $menu, array $settings, array $rewards, array $bonusRewards): void {
+        $inventory = $menu->getInventory();
+    
+        $timerSlots = $settings['timer-slots'] ?? [];
+        $rewardSlots = $settings['rewards-slots'] ?? [];
+        $bonusRewardSlots = $settings['bonus-rewards-slots'] ?? [];
+        $totalTime = $settings['time'] ?? 5; 
+        $soundSettings = $settings['sound'] ?? [];
+    
+        Loader::getInstance()->getScheduler()->scheduleRepeatingTask(
+            new LootboxTask($player, $inventory, $timerSlots, $rewardSlots, $bonusRewardSlots, $rewards, $bonusRewards, $totalTime, $soundSettings),
+            1
+        );
+    
+        $menu->setInventoryCloseListener(function(Player $player, Inventory $inventory) use ($rewardSlots, $bonusRewardSlots): void {
+            $finalRewards = Utils::extractRewardsFromSlots($inventory, $rewardSlots);
+            $finalBonusRewards = Utils::extractRewardsFromSlots($inventory, $bonusRewardSlots);
+            $allRewards = array_merge($finalRewards, $finalBonusRewards);
+    
+            Utils::giveFinalRewards($player, $allRewards);
+        });
+    }
+    
+    /**
+     * Helper method to extract items from specific inventory slots.
+     */
+    public static function extractRewardsFromSlots(Inventory $inventory, array $slots): array
+    {
+        $rewards = [];
+        foreach ($slots as $slot) {
+            $item = $inventory->getItem($slot);
+            if (!$item->isNull()) {
+                $rewards[] = $item;
+            }
+        }
+        return $rewards;
+    }
+
+    /**
+     * Gives the final rewards to the player by adding items to their inventory.
+     */
+    public static function giveFinalRewards(Player $player, array $rewards): void
+    {
+        foreach ($rewards as $reward) {
+            if ($reward instanceof Item) {
+                $player->getInventory()->addItem($reward);
+            }
+        }
+
+        // TODO: Make into the broadcast message.
+        $player->sendMessage("You have received your rewards!");
+    }
+
+     /**
+     * @param Entity $player
+     * @param string $sound
+     * @param int $volume
+     * @param int $pitch
+     * @param int $radius
+     */
+    public static function playSound(Entity $player, string $sound, $volume = 1, $pitch = 1, int $radius = 5): void
+    {
+        foreach ($player->getWorld()->getNearbyEntities($player->getBoundingBox()->expandedCopy($radius, $radius, $radius)) as $p) {
+            if ($p instanceof Player) {
+                if ($p->isOnline()) {
+                    $spk = new PlaySoundPacket();
+                    $spk->soundName = $sound;
+                    $spk->x = $p->getLocation()->getX();
+                    $spk->y = $p->getLocation()->getY();
+                    $spk->z = $p->getLocation()->getZ();
+                    $spk->volume = $volume;
+                    $spk->pitch = $pitch;
+                    $p->getNetworkSession()->sendDataPacket($spk);
+                }
+            }
+        }
+    }
+    
 }
